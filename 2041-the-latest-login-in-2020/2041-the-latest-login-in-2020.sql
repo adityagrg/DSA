aeeebdf6ef547ed3c003ae86c0fe3988262cac32
Aditya Garg <31734655+adityagrg@users.noreply.github.com>
2025-02-03 09:14:33 +0530

    Time: 1081 ms (51.5%), Space: 0B (100%) - LeetHub

--- 2041-the-latest-login-in-2020/2041-the-latest-login-in-2020.sql
+++ 2041-the-latest-login-in-2020/2041-the-latest-login-in-2020.sql
@@ -4,7 +4,6 @@ from
 (select user_id, time_stamp as last_stamp,
 row_number() over (partition by user_id order by time_stamp desc) as rno
 from
-(select user_id, time_stamp
-from Logins
-having year(time_stamp) = 2020) e) f
+Logins
+where year(time_stamp) = 2020) e
 where rno = 1;
