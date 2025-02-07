e9379a2fe802870d464838517121dc5caff27654
Aditya Garg <31734655+adityagrg@users.noreply.github.com>
2025-02-07 12:01:03 +0530

    Time: 1736 ms (6.62%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -87,16 +87,36 @@
 -- and l1.id <> l2.id) t1
 -- group by num
 -- having count(num) > 1;
-select distinct ConsecutiveNums
-from
-(
-select
-case when num = lead1 and num = lead2 then num
-end as ConsecutiveNums
-from
-(select id, num,
-lead(num) over (order by id) as lead1,
-lead(num, 2) over (order by id) as lead2
-from Logs) e
-) f
-where ConsecutiveNums is not null;
+-- select distinct ConsecutiveNums
+-- from
+-- (
+-- select
+-- case when num = lead1 and num = lead2 then num
+-- end as ConsecutiveNums
+-- from
+-- (select id, num,
+-- lead(num) over (order by id) as lead1,
+-- lead(num, 2) over (order by id) as lead2
+-- from Logs) e
+-- ) f
+-- where ConsecutiveNums is not null;
+
+-- SELECT DISTINCT num AS ConsecutiveNums
+-- FROM (
+--     SELECT num,
+--            LEAD(num, 1) OVER (ORDER BY id) AS lead1,
+--            LEAD(num, 2) OVER (ORDER BY id) AS lead2
+--     FROM Logs
+-- ) e
+-- WHERE num = lead1 AND num = lead2;
+
+
+with e as
+(select l1.id as id1, l1.num, l2.id as id2
+from Logs l1
+join logs l2
+on l1.num = l2.num
+and l2.id - l1.id = 1)
+select distinct e1.num as ConsecutiveNums from e e1
+join e e2 on e1.num = e2.num
+and e2.id2 - e1.id1 = 2
