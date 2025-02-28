c364170db282ab82993cbd1162d903ef7cc111d1
Aditya Garg <31734655+adityagrg@users.noreply.github.com>
2025-02-28 15:51:19 +0530

    Time: 0 ms (100%), Space: 18.4 MB (86.91%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -6,25 +6,47 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        # if root == None:
+        #     return []
+        
+        # q1 = deque([root])
+        # q2 = deque()
+        # ans = []
+
+        # current = []
+        # while q1:
+        #     element = q1.popleft()
+        #     current.append(element.val)
+        #     if element.left != None:
+        #         q2.append(element.left)
+        #     if element.right != None:
+        #         q2.append(element.right)
+
+        #     if not q1:
+        #         q1, q2 = q2, q1
+        #         ans.append(current)
+        #         current = []
+
+        # return ans
+    
         if root == None:
             return []
         
-        q1 = deque([root])
-        q2 = deque()
+        q = deque([root])
         ans = []
 
-        current = []
-        while q1:
-            element = q1.popleft()
-            current.append(element.val)
-            if element.left != None:
-                q2.append(element.left)
-            if element.right != None:
-                q2.append(element.right)
-
-            if not q1:
-                q1, q2 = q2, q1
-                ans.append(current)
-                current = []
-
+        while q:
+            noOfElements = len(q)
+            current = []
+            for i in range(noOfElements):
+                element = q.popleft()
+                current.append(element.val)
+                if element.left != None:
+                    q.append(element.left)
+                if element.right != None:
+                    q.append(element.right)
+            
+            ans.append(current)
+        
         return ans
+
